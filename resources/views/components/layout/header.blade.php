631ad633d8de94fbc6d187170d42d83e8238160d
richard <TU_USUARIO@users.noreply.github.com>
2025-10-18 23:47:45 -0400

    refactor header component: improve structure and accessibility of navigation and icons

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,9 +1,11 @@
 @vite('resources/css/app.css')
-<header class=" bg-heading text-white flex px-5 max-h-20">
+<header class=" bg-heading text-white flex px-15 max-h-20">
   <div class=" max-w-8xl flex justify-between items-center w-full ">
-    <img src="{{ asset('images/LogoTurboParts.png') }}" alt="Tutbo Parts Logo" class="w-20">
-    <nav>
-      <ul class="flex gap-4">
+    <a href="home" aria-label="Ir al inicio" >
+      <img src="{{ asset('images/LogoTurboParts.png') }}" alt="Turbo Parts" class="w-20">
+    </a>
+    <nav aria-label="Menú principal">
+      <ul class="flex gap-4 font-semibold">
         <li>
           <a href="Home">Inicio</a>
         </li>
@@ -15,28 +17,22 @@
         </li>
       </ul>
     </nav>
-    <div class="flex gap-4">
+    <div class="flex gap-2 items-center">
       <div class="flex gap-1.5 items-center">
-        <div class="bg-icons rounded-full w-10 h-10 flex justify-center items-center border-border-icons border-1">
-          <a href="">
-            <img class=" w-6 h-6" src=" {{ asset('images/icons/profile.svg') }} " alt="">
-          </a>
-        </div>
+        <a href="" aria-label="Ir a iniciar sesión" class="bg-icons rounded-full w-10 h-10 flex justify-center items-center border-border-icons border-1">
+          <img class=" w-6 h-6" src=" {{ asset('images/icons/profile.svg') }} " alt="" aria-hidden="true">
+        </a>
         <div>
           <p class=" text-xs font-normal opacity-75">Iniciar Sesion</p>
-          <p class="text-xs font-semibold">Cuenta</p>
+          <span class="text-xs font-semibold">Cuenta</span>
         </div>
       </div>
-      <div class="bg-icons rounded-full w-10 h-10 flex justify-center items-center border-border-icons border-1">
-        <a href="">
-          <img class=" w-6" src=" {{ asset('images/icons/favorite.svg') }} " alt=""> 
-        </a>
-      </div>
-      <div class="bg-icons rounded-full w-10 h-10 flex justify-center items-center border-border-icons border-1">
-        <a href="">
-          <img class=" w-6" src=" {{ asset('images/icons/cart.svg') }} " alt="">
-        </a>
-      </div>
+      <a href="" aria-label="Ver favoritos" class="bg-icons rounded-full w-10 h-10 flex justify-center items-center border-border-icons border-1">
+        <img class=" w-6" src=" {{ asset('images/icons/favorite.svg') }} " alt="" aria-hidden="true"> 
+      </a>
+      <a href="" aria-label="Ver carrito de compras" class="bg-icons rounded-full w-10 h-10 flex justify-center items-center border-border-icons border-1">
+        <img class=" w-6" src=" {{ asset('images/icons/cart.svg') }} " alt="" aria-hidden="true">
+      </a>
     </div>
   </div>
 </header>
