0396bf70080db76d1571e8ba610f553525c7db41
richard <TU_USUARIO@users.noreply.github.com>
2025-10-18 23:47:14 -0400

    add footer component: create footer.blade.php with contact and social media links

---
+++ resources/views/components/layout/footer.blade.php
@@ -0,0 +1,100 @@
+@vite('resources/css/app.css')
+<footer class=" bg-footer px-5 text-white"> 
+  <div class="max-w-8xl flex-col justify-between items-center w-full ">
+    <div class="flex justify-around py-14">
+      <address class="flex flex-col gap-5 not-italic">
+        <h3 class=" font-bold">Contactanos</h3>
+        <ul class="flex flex-col gap-6 text-sm ml-9">
+          <li>
+            <h4 class="font-bold mb-1">Numero de telefono</h4>
+            <a href="tel:+59177912729" class=" text-txt-footer hover:underline hover:underline-offset-3 transition-all duration-100">+591 77912729</a>
+          </li>
+          <li>
+            <h4 class="font-bold mb-1">E-Mail</h4>
+            <a href="mailto:turboparts@gmail.com" class="text-txt-footer hover:underline hover:underline-offset-3 transition-all duration-100">turboparts@gmail.com</a>
+          </li>
+          <li>
+            <h4 class="font-bold mb-1">Direccion</h4>
+            <a href="" class="text-txt-footer hover:underline hover:underline-offset-3 transition-all duration-100">Calle 123, Ciudad, Pais</a>
+          </li>
+        </ul>
+      </address>
+
+      <section class="flex flex-col gap-5">
+        <h3 class=" font-bold">Dejanos ayudarte</h3>
+        <ul class="flex flex-col gap-1.5">
+          <li>
+            <a href="" class="text-txt-footer hover:underline hover:underline-offset-3 transition-all duration-100">Devoluciones</a>
+          </li>
+          <li>
+            <a href="" class="text-txt-footer hover:underline hover:underline-offset-3 transition-all duration-100">Politicas de Privacidad</a>
+          </li>
+          <li>
+            <a href="" class="text-txt-footer hover:underline hover:underline-offset-3 transition-all duration-100">Terminos y Condiciones</a>
+          </li>
+        </ul>
+      </section>
+
+      <section class="flex flex-col gap-5">
+        <h3 class=" font-bold">Conocenos</h3>
+        <ul class="flex flex-col gap-1.5">
+          <li>
+            <a href="" class="text-txt-footer hover:underline hover:underline-offset-3 transition-all duration-100">Acerca de Turbo Parts</a>
+          </li>
+          <li>
+            <a href="" class="text-txt-footer hover:underline hover:underline-offset-3 transition-all duration-100">Comentarios de Clientes</a>
+          </li>
+          <li>
+            <a href="" class="text-txt-footer hover:underline hover:underline-offset-3 transition-all duration-100">Responsabilidad Social</a>
+          </li>
+        </ul>
+      </section>
+    </div>
+
+    <div class=" flex justify-between items-center border-y-2 w-6xl mx-auto border-icons">
+      <div>
+        <img src="{{ asset('images/LogoTurboParts.png') }}" alt="Turbo Parts" class="w-20">
+      </div>
+      <nav class="flex items-center gap-3 ">
+        <p class=" font-semibold text-xs text-txt-footer">Siguenos:</p>
+        <ul class="flex items-center gap-1">
+          <li>
+            <a href="#" aria-label="Facebook" target="_blank" rel="noopener noreferrer"  class=" bg-icons w-10 h-10 rounded-full flex justify-center items-center">
+              <img src="{{ asset('images/icons/facebook.svg') }}" class=" w-icon-footer h-icon-footer" alt="" aria-hidden="true">
+            </a>
+          </li>
+          <li>
+            <a href="#" aria-label="Twitter" target="_blank" rel="noopener noreferrer"  class=" bg-icons w-10 h-10 rounded-full flex justify-center items-center">
+              <img src="{{ asset('images/icons/twitter.svg') }}" class=" w-3.5 h-3" alt="" aria-hidden="true">
+            </a>
+          </li>
+          <li>
+            <a href="#" aria-label="Instagram" target="_blank" rel="noopener noreferrer" class=" bg-icons w-10 h-10 rounded-full flex justify-center items-center">
+              <img src="{{ asset('images/icons/instagram.svg') }}" class=" w-4 h-4" alt="" aria-hidden="true">
+            </a>
+          </li>
+          <li>
+            <a href="#" aria-label="YouTube" target="_blank" rel="noopener noreferrer" class=" bg-icons w-10 h-10 rounded-full flex justify-center items-center">
+              <img src="{{ asset('images/icons/youtube.svg') }}" class=" w-3.5 h-3.5" alt="" aria-hidden="true">
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+    <div class="flex justify-between items-center w-6xl mx-auto py-4">
+      <small class="opacity-50">Copyright 2025 © Turbo Parts Theme. All right reserved.</small>
+      <ul class="flex items-center gap-3">
+        <li>
+          <img src="{{ asset('images/icons/mastercard.svg') }}" alt="Mastercard">
+        </li>
+        <li>
+          <img src="{{ asset('images/icons/visa.svg') }}" alt="Visa">
+        </li>
+        <li>
+          <img src="{{ asset('images/icons/paypal.svg') }}" alt="PayPal">
+        </li>
+      </ul>
+    </div>
+  </div>
+</footer>
