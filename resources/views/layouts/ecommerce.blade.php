7ac53c08c300a62e743a18fc30496d99b86507c6
richard <TU_USUARIO@users.noreply.github.com>
2025-10-22 13:45:19 -0400

    add ecommerce layout: create ecommerce.blade.php for main page structure

---
+++ resources/views/layouts/ecommerce.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Mi Tienda')</title>
+
+    @vite('resources/css/app.css')
+</head>
+<body>
+  <x-layout.header />
+
+  <main class="bg-main-primary">
+    @yield('content')
+  </main>
+
+  <x-layout.footer />
+</body>
+</html>
