b6f78be9733023709467761abfbb8847a024e9f1
richard <TU_USUARIO@users.noreply.github.com>
2025-10-25 17:11:39 -0400

    A section was created to display the services of the orders we have in the store.

---
+++ resources/views/components/sections/services_orders.blade.php
@@ -0,0 +1,10 @@
+<section class=" bg-gray-200/60 rounded-md flex flex-col gap-4 p-5">
+  <div class=" flex gap-3 items-center">
+    <img class=" w-4 h-4" src=" {{ asset('images/icons/orders.svg') }} " alt="">
+    <p class=" font-normal">Envío en <strong>24 horas</strong>: Su producto será enviado rápidamente.</p>
+  </div>
+  <div class=" flex gap-3 items-center">
+    <img class=" w-4 h-4" src=" {{ asset('images/icons/guarantee.svg') }} " alt="">
+    <p class=" font-normal">Garantía de <strong>3 años</strong>: Turbo Parts es seguro bajo las condiciones de la garantía.</p>
+  </div>
+</section>
