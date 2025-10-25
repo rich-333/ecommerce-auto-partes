da31cc7a034df3ace612ffb7cc5b38e0bbd0cb71
richard <TU_USUARIO@users.noreply.github.com>
2025-10-25 16:31:59 -0400

    add pagination component: create pagination.blade.php for navigation links

---
+++ resources/views/components/pagination.blade.php
@@ -0,0 +1,17 @@
+<nav class=" flex gap-2 justify-center mt-5">
+  <a class="pagination__active cursor-pointer p-2 px-4 rounded-lg font-semibold hover:bg-discount hover:text-white transition-all duration-100" href="">1</a>
+  <a class="cursor-pointer p-2 px-4 rounded-lg font-semibold hover:bg-discount hover:text-white transition-all duration-100" href="">2</a>
+  <a class="cursor-pointer p-2 px-4 rounded-lg font-semibold hover:bg-discount hover:text-white transition-all duration-100" href="">3</a>
+  <a class="cursor-pointer p-2 px-4 rounded-lg font-semibold hover:bg-discount hover:text-white transition-all duration-100 flex justify-center items-center group" href="">
+    <svg width="11" height="20" viewBox="0 0 11 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path class=" fill-black group-hover:fill-white transition-all duration-100" d="M0.744 19.5247C0.536 19.5247 0.36 19.4567 0.216 19.3207C0.072 19.1847 0 19.0087 0 18.7927C0 18.5767 0.08 18.3887 0.24 18.2287L8.736 9.73266L0.24 1.23666C0.08 1.07666 0 0.908657 0 0.732656C0 0.556656 0.08 0.388657 0.24 0.228657C0.4 0.0686569 0.584 -0.00734329 0.792 0.000656128C1 0.00865746 1.184 0.0846558 1.344 0.228657L10.344 9.22866C10.488 9.38866 10.56 9.57266 10.56 9.78066C10.56 9.98866 10.488 10.1727 10.344 10.3327L1.344 19.3327L1.272 19.3567C1.032 19.4687 0.856 19.5247 0.744 19.5247Z" />
+    </svg>
+  </a>
+</nav>
+
+<style>
+  .pagination__active {
+    background-color: var(--color-discount);
+    color: white;
+  }
+</style>
