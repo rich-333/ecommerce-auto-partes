f90fb3a3f2d792628c940f0896b21bf446be9601
richard <TU_USUARIO@users.noreply.github.com>
2025-10-25 16:39:22 -0400

    A component was created to display the total cart payment

---
+++ resources/views/components/sections/cart_totals.blade.php
@@ -0,0 +1,24 @@
+<section>
+  <h2 class=" text-xl font-medium">Total del carrito</h2>
+
+  <div class=" mt-4">
+    <div class=" flex justify-between items-center border-b-2 border-carousel-border py-3">
+      <p class=" text-sm font-semibold">Subtotal</p>
+      <p class=" font-normal">447 Bs.</p>
+    </div>
+
+    <div class=" flex justify-between items-center border-b-2 border-carousel-border py-3">
+      <p class=" text-sm font-semibold">Envio</p>
+      <p class=" font-normal">15 Bs.</p>
+    </div>
+
+    <div class=" flex justify-between items-center py-3">
+      <p class=" text-sm font-semibold">Total</p>
+      <p class=" font-bold">447 Bs.</p>
+    </div>
+  </div>
+
+  <button class=" bg-discount text-white font-semibold w-full mt-2 rounded-lg py-3 cursor-pointer">
+    Proceder al pago
+  </button>
+</section>
