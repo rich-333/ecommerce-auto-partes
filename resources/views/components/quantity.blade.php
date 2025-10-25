b2f948189cac4dae8f36b62329280b6608cde72a
richard <TU_USUARIO@users.noreply.github.com>
2025-10-25 16:35:18 -0400

    A quantity component was created to be able to manipulate the number of products to purchase.

---
+++ resources/views/components/quantity.blade.php
@@ -0,0 +1,7 @@
+<div>
+  <div class=" flex items-center border-2 border-carousel-border rounded-lg shadow-2xs w-28 h-12">
+    <button class=" py-2 px-2 flex-1 cursor-pointer">-</button>
+    <p class=" flex-1 px-3 text-center font-bold">1</p>
+    <button class="flex-1 p-2 cursor-pointer">+</button>
+  </div>
+</div>
