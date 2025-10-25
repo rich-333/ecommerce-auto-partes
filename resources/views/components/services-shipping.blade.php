691003de4c4aef2e7d0f9b02f623411d8a541870
richard <TU_USUARIO@users.noreply.github.com>
2025-10-25 16:36:28 -0400

    A component was created to display shipping services and make them easier to reuse.

---
+++ resources/views/components/services-shipping.blade.php
@@ -0,0 +1,6 @@
+<article class="bg-gray-200/30 rounded-md py-10 px-7">
+  <img class="w-12 h-12 mx-auto mb-4" src="{{ asset($icon) }}" alt="Icono de {{ $title }}">
+  
+  <h3 class="text-center font-semibold">{{ $title }}</h3>
+  <p class="text-center font-normal text-sm text-gray-600">{{ $description }}</p>
+</article>
