12f616a5bca9acc1945586f0d8b4cedc76bfaa3d
richard <TU_USUARIO@users.noreply.github.com>
2025-10-25 17:05:11 -0400

    A section was added to list all the products that are in the spare parts store to show them to customers.

---
+++ resources/views/components/sections/product_list.blade.php
@@ -0,0 +1,15 @@
+<section class="">
+  <header class=" flex py-8">
+    <p class=" font-normal text-secondary">Mostrando 1-12 de 60 resultados</p>
+  </header>
+
+  <div class=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-x-18 gap-y-10">
+    <x-product />
+    <x-product />
+    <x-product />
+    <x-product />
+    <x-product />
+  </div>
+
+  <x-pagination />
+</section>
