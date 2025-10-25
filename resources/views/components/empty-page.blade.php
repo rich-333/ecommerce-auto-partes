40f702ce47451cc13e40a8ee0ebdae680314654c
richard <TU_USUARIO@users.noreply.github.com>
2025-10-25 16:31:49 -0400

    add empty page component: create empty-page.blade.php for displaying a placeholder layout

---
+++ resources/views/components/empty-page.blade.php
@@ -0,0 +1,10 @@
+<section class=" w-full my-7 flex flex-col items-center gap-10">
+  <div class="w-44 h-44 bg-heading flex items-center justify-center rounded-full">
+    <img class="w-28 h-28 mx-auto" src="{{ $icon }}" alt="">  
+  </div>
+
+  <div class=" flex flex-col items-center gap-4">
+    <p class=" font-normal text-center">{{ $description }} </p>
+    <a class=" font-semibold px-5 py-2.5 bg-discount text-white rounded-lg" href="#">Volver a la tienda</a>
+  </div>
+</section>
